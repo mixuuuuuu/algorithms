f86baf89fe1b63ce93c907aacc86857a05816ba8
mixuuuuuu <mixuuuuuuuuuu@gmail.com>
2024-04-16 15:32:35 +0800

    new file:   nifix_to_postfix/.vs/nifix_to_postfix/FileContentIndex/0cc770d5-4a3c-4930-b966-136845e6bfc8.vsidx 	new file:   nifix_to_postfix/.vs/nifix_to_postfix/FileContentIndex/42358cd3-abdb-45c7-b89b-4ffd3f773fb3.vsidx 	new file:   nifix_to_postfix/.vs/nifix_to_postfix/FileContentIndex/read.lock 	new file:   nifix_to_postfix/.vs/nifix_to_postfix/v17/.suo 	new file:   nifix_to_postfix/.vs/nifix_to_postfix/v17/Browse.VC.db 	new file:   nifix_to_postfix/.vs/nifix_to_postfix/v17/ipch/AutoPCH/43f6379aea132573/NIFIX_TO_POSTFIX.ipch 	new file:   nifix_to_postfix/.vs/nifix_to_postfix/v17/ipch/AutoPCH/c161691418d58795/INFIX_TO_POSTFIX.ipch 	new file:   nifix_to_postfix/infix_to_postfix.cpp 	new file:   nifix_to_postfix/nifix_to_postfix.sln 	new file:   nifix_to_postfix/nifix_to_postfix.vcxproj 	new file:   nifix_to_postfix/nifix_to_postfix.vcxproj.filters 	new file:   nifix_to_postfix/nifix_to_postfix.vcxproj.user 	new file:   nifix_to_postfix/x64/Debug/infix_to_postfix.obj 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.exe 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.exe.recipe 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.ilk 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.log 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.pdb 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.tlog/CL.command.1.tlog 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.tlog/CL.read.1.tlog 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.tlog/CL.write.1.tlog 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.tlog/link.command.1.tlog 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.tlog/link.read.1.tlog 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.tlog/link.write.1.tlog 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.tlog/nifix_to_postfix.lastbuildstate 	new file:   nifix_to_postfix/x64/Debug/nifix_to_postfix.vcxproj.FileListAbsolute.txt 	new file:   nifix_to_postfix/x64/Debug/vc143.idb 	new file:   nifix_to_postfix/x64/Debug/vc143.pdb 	new file:   postfix/.vs/postfix/FileContentIndex/783eca9f-c22a-4a1e-80e5-a73228308865.vsidx 	new file:   postfix/.vs/postfix/FileContentIndex/80b00420-b9ba-40f3-a411-8cbfc95567dd.vsidx 	new file:   postfix/.vs/postfix/FileContentIndex/read.lock 	new file:   postfix/.vs/postfix/v17/.suo 	new file:   postfix/.vs/postfix/v17/Browse.VC.db 	new file:   postfix/.vs/postfix/v17/ipch/AutoPCH/3b226a8e8b746dbb/POSTFIX.ipch 	new file:   postfix/postfix.cpp 	new file:   postfix/postfix.sln 	new file:   postfix/postfix.vcxproj 	new file:   postfix/postfix.vcxproj.filters 	new file:   postfix/postfix.vcxproj.user 	new file:   postfix/x64/Debug/postfix.exe 	new file:   postfix/x64/Debug/postfix.exe.recipe 	new file:   postfix/x64/Debug/postfix.ilk 	new file:   postfix/x64/Debug/postfix.log 	new file:   postfix/x64/Debug/postfix.obj 	new file:   postfix/x64/Debug/postfix.pdb 	new file:   postfix/x64/Debug/postfix.tlog/CL.command.1.tlog 	new file:   postfix/x64/Debug/postfix.tlog/CL.read.1.tlog 	new file:   postfix/x64/Debug/postfix.tlog/CL.write.1.tlog 	new file:   postfix/x64/Debug/postfix.tlog/link.command.1.tlog 	new file:   postfix/x64/Debug/postfix.tlog/link.read.1.tlog 	new file:   postfix/x64/Debug/postfix.tlog/link.write.1.tlog 	new file:   postfix/x64/Debug/postfix.tlog/postfix.lastbuildstate 	new file:   postfix/x64/Debug/postfix.vcxproj.FileListAbsolute.txt 	new file:   postfix/x64/Debug/vc143.idb 	new file:   postfix/x64/Debug/vc143.pdb

---
+++ nifix_to_postfix/infix_to_postfix.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+#include <sstream>
+
+using namespace std;
+
+int precedence(char op) {
+	if (op == '+' || op == '-') {
+		return 1;
+	}
+	else if (op == '*' || op == '/') {
+		return 2;
+	}
+	return 0; //括号返回0
+}
+
+bool isOperator(char c) {
+    return (c == '+') || (c == '-') || (c == '*') || (c == '/') || (c == '(') || (c == ')');
+}
+
+string infixToPostfix(const string& infix) {
+    stack<char> operands;
+    stringstream ss(infix);
+    string tempNum;
+    string postfix;
+    //stringstream postfix;
+    char token;
+    while (ss>>token) {
+        if (isdigit(token)) {
+            //如果是数字，则添加到后缀表达式末尾
+            tempNum.push_back(token);
+        }
+        else {
+            if (!tempNum.empty()) {
+                postfix += tempNum + ' ';
+                tempNum.clear();
+            }
+            if (token == '(') {
+                operands.push(token);
+            }
+            else if (token == ')') {
+                while (!operands.empty() && operands.top() != '(') {
+                    postfix.push_back(operands.top());
+                    postfix.push_back(' ');
+                    operands.pop();
+                }
+                if (operands.top() == '(') {
+                    operands.pop();
+                }
+            }
+            else if (token == '=') {
+                continue;
+            }
+            else {
+                //如果是运算符
+                while (!operands.empty() && precedence(operands.top()) >= precedence(token)) {
+                    postfix.push_back(operands.top());
+                    postfix.push_back(' ');
+                    operands.pop();
+                }
+                operands.push(token);
+            }
+        }
+    }
+    if (!tempNum.empty()) {
+        postfix += tempNum + ' ';
+        tempNum.clear();
+    }
+    while (!operands.empty()) {
+        postfix.push_back(operands.top());
+        postfix.push_back(' ');
+        operands.pop();
+    }
+    return postfix;
+}
+
+int main()
+{
+    string infix;
+    getline(cin, infix);
+    string postfix = infixToPostfix(infix);
+    cout << postfix << endl;
+	return 0;
+}
---
+++ postfix/postfix.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+bool isOperator(char c) {
+    return (c == '+') || (c == '-') || (c == '*') || (c == '/');
+}
+
+int operation(int operand1, int operand2, char op) {
+    switch (op) {
+        case'+':
+            return operand1 + operand2;
+        case'-':
+            return operand1 - operand2;
+        case'*':
+            return operand1 * operand2;
+        case'/':
+            return operand1 / operand2;
+        default:
+            cerr << "Error:Invalid operator!" << endl;
+            return 0;
+    }
+}
+
+int evaluatePostfixExpression(const string& postfix) {
+    stack<int> operands;
+    stringstream ss(postfix);
+    string token;
+    while (ss >> token) {
+        if (!isOperator(token[0]) && token[0] != '=') {
+            //如果是数字，则转换为整数，入栈
+            operands.push(stoi(token));
+        }
+        else if(token[0] != '=') {
+            //如果是运算符，弹出两个数，运算后将结果入栈
+            int operand2 = operands.top();
+            operands.pop();
+            int operand1 = operands.top();
+            operands.pop();
+            operands.push(operation(operand1, operand2, token[0]));
+        }
+        if (token[0] == '=') {
+            return operands.top();
+        }
+    }
+    return operands.top();
+}
+
+int main()
+{ 
+    string postfix;
+    getline(cin, postfix);
+    int result = evaluatePostfixExpression(postfix);
+    cout << result << endl;
+    return 0;
+}
